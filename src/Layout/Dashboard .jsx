be4418c20c0f8f82925f153e0aaff10e21c4c8d7
nayeem-miah <nayeem5113a@gmail.com>
2024-06-21 06:02:17 +0600

    responsive-dashbord

--- src/Layout/Dashboard .jsx
+++ src/Layout/Dashboard .jsx
@@ -8,73 +8,92 @@ import { NavLink, Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import useAuth from "../Hooks/useAuth";
 import useAdmin from "../Hooks/useAdmin";
+import { useState } from "react";
 
 const Dashboard = () => {
   const { user } = useAuth();
   const [isAdmin] = useAdmin();
-  
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
     <div>
-      <Navbar></Navbar>
-      {/* dashBoard content  */}
-      <div className="flex drawer drawer-content lg:drawer-open ">
-        <div className="w-64  min-h-screen bg-purple-500 mt-16">
-          <ul className="menu text-black font-bold p-4 ">
+     <div className=" sm:hidden lg:block"> <Navbar /></div>
+      {/* Dashboard content */}
+      <div className="flex flex-col lg:flex-row min-h-screen ">
+        {/* Toggle button for small screens */}
+        <div className="lg:hidden p-4 border">
+          <button
+            className="text-white bg-purple-500 p-2 rounded"
+            onClick={toggleDrawer}
+          >
+            {isDrawerOpen ? "Close Menu" : "Open Menu"}
+          </button>
+        </div>
+
+        {/* Sidebar */}
+        <div
+          className={`w-full lg:w-64 bg-purple-500 lg:fixed lg:min-h-full  lg:mt-0 ${
+            isDrawerOpen ? "block" : "hidden"
+          } lg:block`}
+        >
+          <ul className="menu text-black font-bold p-4 mt-24">
             {isAdmin && (
               <>
                 <li>
-                  <NavLink to={"/dashboard/adminHome"}>
-                    <FaHouseMedical></FaHouseMedical>
+                  <NavLink to="/dashboard/adminHome">
+                    <FaHouseMedical />
                     Admin Home
                   </NavLink>
                 </li>
-
                 <li>
-                  <NavLink to={"/dashboard/users"}>
-                    <FaUsers></FaUsers>
+                  <NavLink to="/dashboard/users">
+                    <FaUsers />
                     All Users
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to={"/dashboard/allArticlesAdmin"}>
-                    <FaRegNewspaper></FaRegNewspaper>
+                  <NavLink to="/dashboard/allArticlesAdmin">
+                    <FaRegNewspaper />
                     All Articles
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to={"/dashboard/allPublisher"}>
-                    <FaGalacticRepublic></FaGalacticRepublic>
+                  <NavLink to="/dashboard/allPublisher">
+                    <FaGalacticRepublic />
                     Add Publisher
                   </NavLink>
                 </li>
               </>
             )}
-
-            <div className=" min-h-52"></div>
-
+            <div className="min-h-52"></div>
             <li>
-              <NavLink to={"/"}>
-                <FaHouseMedical></FaHouseMedical>
+              <NavLink to="/">
+                <FaHouseMedical />
                 Home
               </NavLink>
             </li>
-
             <li>
               <p>
                 {user && (
                   <img
                     src={user?.photoURL}
                     className="w-10 h-10 rounded-full"
+                    alt="User"
                   />
                 )}
                 Admin Profile
               </p>
             </li>
           </ul>
-          {/* sidebar content */}
         </div>
-        <div className="flex-1 px-8 drawer-content">
-          <Outlet></Outlet>
+
+        {/* Content Area */}
+        <div className="flex-1 lg:ml-64 p-4">
+          <Outlet />
         </div>
       </div>
     </div>
